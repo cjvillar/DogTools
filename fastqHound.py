432f53a3537696b514ea68e0eb94492fd27f62ee
cjvillar <cjvillar@ucsc.edu>
2019-03-15 12:52:03 -0700

    added new version of fastqHound.py to test git

--- fastqHound.py
+++ fastqHound.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
-import os,sys,gzip
+import os,sys,gzip,argparse
+
+from argparse import ArgumentParser
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'infile',
+        type=argparse.FileType('rb'),
+        default=sys.stdin,
+    )
+
+    args = parser.parse_args()
+
+
 # handles file input
-inFile = sys.argv[1]
+#inFile = sys.argv[1]
+
+
 #reads number of lines in a gzipped file
-with gzip.open(inFile, 'rb') as f:
+with gzip.open(infile, 'rb') as f:
  row = len(f.readlines())   
 print("This file has " + '\x1b[6;30;42m' + str(row) + '\x1b[0m'+ " lines")
 
@@ -14,7 +31,6 @@ print("Here are the first 4 lines of " + '\x1b[6;30;42m' + inFile + '\x1b[0m')
 with gzip.open(inFile,'r') as f:
 	for i in xrange(4):
 	  print f.readline()
-
 
 
 
